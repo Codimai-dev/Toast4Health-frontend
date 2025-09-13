40e0c30be7fefa2412b7aba48cc6e51cec26ebab
Nagaraj N P <nagarajnp@2004gmail.com>
2025-09-13 12:32:02 +0530

    equipment package added

--- assets/Javascript/equipment.js
+++ assets/Javascript/equipment.js
@@ -1,23 +1,20 @@
-const btns = document.querySelectorAll(".key-btn");
-const buyBtns = document.querySelectorAll(".buy");
-const rentBtns = document.querySelectorAll(".rent");
-btns.forEach((btn)=>{
-    btn.addEventListener("click",(e)=>{
-        if(btn.classList.contains('Buy')){
-            buyBtns.forEach(buy=>{
-                buy.classList.remove('hidden');
-                   rentBtns.forEach(rent=>{
-                rent.classList.add('hidden');
-                
-        })
-        })
-        }else{
-             rentBtns.forEach(rent=>{
-                rent.classList.remove('hidden');
-                 buyBtns.forEach(buy=>{
-                buy.classList.add('hidden');
-        })
-        })
-        }
-    })
+const buy = document.querySelector('.buy');
+const rent = document.querySelector('.rent');
+const buySection = document.querySelector("#buy-equipment");
+const rentSection = document.querySelector("#rent-equipment");
+
+buy.addEventListener("click",()=>{
+    if(buySection.classList.contains("hidden")){
+        buySection.classList.remove("hidden");
+    }
+    buy.classList.add("active")
+    rentSection.classList.add("hidden");
+         rent.classList.remove("active")
+})
+rent.addEventListener("click",()=>{
+    if(rentSection.classList.contains("hidden"))
+        rentSection.classList.remove("hidden");
+    rent.classList.add("active")
+     buy.classList.remove("active")
+    buySection.classList.add("hidden");
 })
