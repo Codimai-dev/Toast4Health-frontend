eada69457b3cc4f5682811d319716c55ffa16939
Nagaraj N P <nagarajnp@2004gmail.com>
2025-09-19 21:56:14 +0530

    Comprehensive Health Checkupa added

--- assets/Javascript/lab.js
+++ assets/Javascript/lab.js
@@ -2,6 +2,7 @@ const buttons = document.querySelectorAll('.tab');
 const DiagnosticTest = document.querySelector("#Diagnostic-test")
 const AdvanceDiagnostics = document.querySelector("#Advance-diagnostics")
 const tittle = document.querySelector("#tittle");
+const comprehensiveHealthCheckup = document.querySelector("#comprehensive-health-checkup");
 console.log(buttons)
 buttons.forEach(btn => {
     btn.addEventListener("click",()=>{
@@ -11,14 +12,20 @@ buttons.forEach(btn => {
               tittle.innerHTML = "Diagnostics";
             if(DiagnosticTest.classList.contains('hidden'))DiagnosticTest.classList.remove("hidden");
             AdvanceDiagnostics.classList.add("hidden");
+            comprehensiveHealthCheckup.classList.add("hidden")
 
         }else if(btn.classList.contains("Advance-diagnostics")){
              btn.classList.add("active")
                 tittle.innerHTML = "Advance diagnostics";
              if(AdvanceDiagnostics.classList.contains('hidden'))AdvanceDiagnostics.classList.remove("hidden");
             DiagnosticTest.classList.add("hidden");
+            comprehensiveHealthCheckup.classList.add("hidden")
         }else{
-
+            btn.classList.add("active")
+            tittle.innerHTML = "Comprehensive Health Checkup";
+             if(comprehensiveHealthCheckup.classList.contains('hidden'))comprehensiveHealthCheckup.classList.remove("hidden")
+                AdvanceDiagnostics.classList.add("hidden");
+            DiagnosticTest.classList.add("hidden");
         }
     })
 })
